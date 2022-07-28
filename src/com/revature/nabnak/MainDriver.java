ba5ea46bddbaf2df919d07d4c3e5f85b0ef2a14e
Pitmon Foxall <foxthe4th@gmail.com>
2022-07-27 18:05:08 -0600

    Continued work on the Kanban board. We added a register feature, and created the ability for a user to set up an account with password. We still need to set up logingit status!

--- src/com/revature/nabnak/MainDriver.java
+++ src/com/revature/nabnak/MainDriver.java
@@ -1,5 +1,11 @@
 package com.revature.nabnak;
 
+/* Nabnak is a kanban board for professionsl devs aimed to be a decent virtual board.
+    The goal is to allow members to join the application, be assigned part of a team, and share their Kanban cards.
+
+ */
+
+import com.revature.nabnak.models.Member;
 import org.w3c.dom.xpath.XPathNamespace;
 
 import java.io.*;
@@ -7,29 +13,35 @@ import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Locale;
 
-/* Nabnak is a kanban board for professionsl devs aimed to be a decent virtual board.
-    The goal is to allow members to join the application, be assigned part of a team, and share their Kanban cards.
 
- */
 public class MainDriver {
+
+    //We need to receive input from a user!
+    //This instance, BufferedReader, is used to get input from the user via the terminal
+    //DRY Principle = DONT REPEAT YOURSELF
+    static BufferedReader terminalReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
 
+
         //Define the datatype in the array. This will be the only acceptable datatype.
         //Arrays are fixed in size
-        String[] welcomemessages = {"Welcome to Nabnak", "1) Login", "2) Register", "3) Exit Application"};
+        String[] welcomeMessages = {"Welcome to Nabnak", "1) Login", "2) Register", "3) Exit Application"};
 
-        System.out.println(welcomemessages[0]);
-        System.out.println(welcomemessages[1]);
-        System.out.println(welcomemessages[2]);
-        System.out.println(welcomemessages[3]);
+        System.out.println(welcomeMessages[0]);
+        System.out.println(welcomeMessages[1]);
+        System.out.println(welcomeMessages[2]);
+        System.out.println(welcomeMessages[3]);
+        welcomeMessages[3] = "3) Exit Application"; //values at indicies can be reassigned but not mutated
+        //At line 18, the "3) Exit application" is removed from the String and memory via garbage collection
+        //NOTE: GC cannot be forced, but it can be suggested by the command below
+        // System.gc(); //One of those questions
+        System.out.println(welcomeMessages[3]);
+        //System.out.println(welcomeMessages[4]); //This will throw an ArrayIndexOutOfBoundsException
 
-        //We need to receive input from a user!
-        //This instance, BufferedReader, is used to get input from the user via the terminal
-        BufferedReader terminalReader = new BufferedReader(new InputStreamReader(System.in));
 
         //Try-Catch blocks are a major way to handle exceptions
-        try { //try-block leverages risky code
+        try { //try-block leverages risky code that might throw an exception
             String firstInput = terminalReader.readLine();  //This will throw an IO exception. This MUST be caught before the code is compiled
 
            // if (firstInput.equals("1")) {
@@ -45,6 +57,8 @@ public class MainDriver {
            //     } else{
            //         System.out.println("User has not selected anything");
            //     }
+            // If your switch cases don't handle control flow, fall-through will result
+            // This can be advantageous
 
             switch (firstInput){
                 case "1":  // if firstInput.equals("1") then this case will execute
@@ -53,43 +67,17 @@ public class MainDriver {
 
                 case  "2":
                     System.out.println("User has selected register");
-                    System.out.println("Please enter your user email");
-                    String email = terminalReader.readLine();
-
-                    System.out.println("Please enter your first and last name");
-                    String fullName = terminalReader.readLine();
-
-                    System.out.println("Please enter your experience in months");
-                    String experienceMonths = terminalReader.readLine();
-
-                    System.out.println("Please enter your registration date");
-                    String registrationDate = LocalDateTime.now().toString();
-
-                            // This is a form of logging
-                    System.out.printf("New user has registered under \n User:%s,%s,%s,%s", email,fullName, experienceMonths, registrationDate);
-                            // \n prints to a new line
-
-                    File memoryFile = new File("resources/data.txt");
-
-                    // This is a try-with-resources block. This allows for anything that extends to AutoClosable to be auto closed.
-                    try(FileWriter fileWriter = new FileWriter(memoryFile,true);){
-
-                    String output = email + "," + fullName + "," + experienceMonths + "," + registrationDate + "\n";
-                    fileWriter.write(output);
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                    //fileWriter.close();  //be mindful to ALWAYS close!!
-
+                    register(); // register() - the () invokes the method
 
                     break;
 
+                    // case "i": //example of fall through
                 case "3":
                     System.out.println("See you next time!");
                     break;
 
                 default:
-                    System.out.println("User has not selected anything");
+                    System.out.println("User has not selected a valid input");
 
 
 
@@ -97,15 +85,54 @@ public class MainDriver {
 
             } catch(IOException e){  //Catches that exception and assigns to variable e
                 e.printStackTrace();
-
-                //Wrapper classes convert other datatypes to the one in question, as long as it's formatted properly.
-                //Note: If we replace a value in the array, the initial value of the index in question will be removed via GC
-                // GC - Garbage Collection is when Java cleans up unwanted data. This CANNOT be forced
-
             }
         }
 
-        public static void register(){
+        public static void register() throws IOException { // Throws + ExceptionName is known as ducking
+                // Ducking means passing the responsibility of handling the exception to the code that's calling it.
+                // Problem... terminalReader is not within this scope? Solution...
+
+            System.out.println("Please enter your user email: \n>"); // \n is a new line character, aka return or enter
+            String email = terminalReader.readLine();
+
+            System.out.println("Please enter your first and last name \n>");
+            String fullName = terminalReader.readLine();
+
+            System.out.println("Please enter your experience in months \n>");
+            String experienceMonths = terminalReader.readLine();
+
+            System.out.println("Please enter your registration date ");
+            String registrationDate = LocalDateTime.now().toString();
+
+            // This is a form of logging
+            // System.out.printf("New user has registered under \n User:%s,%s,%s,%s", email,fullName, experienceMonths, registrationDate);
+            // printf is a formatter
+
+            File memoryFile = new File("resources/data.txt");
+
+            // This is a try-with-resources block. This allows for anything that extends to AutoClosable to be auto closed.
+            try(FileWriter fileWriter = new FileWriter(memoryFile,true);){
+
+                Member member = new Member(email, fullName, Integer.parseInt(experienceMonths), registrationDate);
+                //This is an old, and rather poor way
+                //member.email = email;
+                //member.fullName = fullName;
+                //member.experienceMonths = Integer.parse(experienceMonths); // Use the primitive's wrapper class to convert
+                //member.registrationDate = registrationDate;
+
+                System.out.println(member.getPassword());
+
+                member.setPassword("12345");
+                System.out.println(member.getPassword());
+
+                System.out.println("New user has registered: " + member.toString());
+
+               Member member2 = new Member();
+                fileWriter.write(member.writetoFile());
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            //fileWriter.close();  //be mindful to ALWAYS close!!
 
 
 
