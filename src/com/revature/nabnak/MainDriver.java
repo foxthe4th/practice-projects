292ca426dd407e8bb98e52ca1a87ac3de77d1ec7
Pitmon Foxall <foxthe4th@gmail.com>
2022-07-28 10:30:26 -0600

    Added a 4th case and a loop for the system to keep running and read the file line by line. Created new method readFile that is called in case 3 of our main menu. I updated the readFile method to include the use of the Member Class … …with an Array instead of using just the String from the direct data.txt output. This will be the final commit for the single-layer application to main.

--- src/com/revature/nabnak/MainDriver.java
+++ src/com/revature/nabnak/MainDriver.java
@@ -21,77 +21,92 @@ public class MainDriver {
     //DRY Principle = DONT REPEAT YOURSELF
     static BufferedReader terminalReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-
+        boolean isRunning = true;
         //Define the datatype in the array. This will be the only acceptable datatype.
         //Arrays are fixed in size
-        String[] welcomeMessages = {"Welcome to Nabnak", "1) Login", "2) Register", "3) Exit Application"};
 
-        System.out.println(welcomeMessages[0]);
-        System.out.println(welcomeMessages[1]);
-        System.out.println(welcomeMessages[2]);
-        System.out.println(welcomeMessages[3]);
-        welcomeMessages[3] = "3) Exit Application"; //values at indicies can be reassigned but not mutated
-        //At line 18, the "3) Exit application" is removed from the String and memory via garbage collection
-        //NOTE: GC cannot be forced, but it can be suggested by the command below
-        // System.gc(); //One of those questions
-        System.out.println(welcomeMessages[3]);
-        //System.out.println(welcomeMessages[4]); //This will throw an ArrayIndexOutOfBoundsException
+        while (isRunning) { //While loop is used when you have no idea how many iterations are required
+            String[] welcomeMessages = {"Welcome to Nabnak", "1) Login", "2) Register", "3) View Members", "4) Exit Application"};
+
+            System.out.println(welcomeMessages[0]);
+            System.out.println(welcomeMessages[1]);
+            System.out.println(welcomeMessages[2]);
+            System.out.println(welcomeMessages[3]);
+            System.out.println(welcomeMessages[4]);
+           // welcomeMessages[3] = "3) Exit Application"; //values at indicies can be reassigned but not mutated
+            //At line 18, the "3) Exit application" is removed from the String and memory via garbage collection
+            //NOTE: GC cannot be forced, but it can be suggested by the command below
+            // System.gc(); //One of those questions
+            //System.out.println(welcomeMessages[3]);
+            //System.out.println(welcomeMessages[4]); //This will throw an ArrayIndexOutOfBoundsException
 
 
-        //Try-Catch blocks are a major way to handle exceptions
-        try { //try-block leverages risky code that might throw an exception
-            String firstInput = terminalReader.readLine();  //This will throw an IO exception. This MUST be caught before the code is compiled
+            //Try-Catch blocks are a major way to handle exceptions
+            try { //try-block leverages risky code that might throw an exception
+                String firstInput = terminalReader.readLine();  //This will throw an IO exception. This MUST be caught before the code is compiled
 
-           // if (firstInput.equals("1")) {
-           //     System.out.println("User has selected login...");
-           // } else if {
-           //     firstInput.equals("2") {
-           //         System.out.println("User has selected Register...");
-           //     } else if {
-           //         firstInput.equals("3") {
-           //             System.out.println("See you next time!");
-           //         }
-            //
-           //     } else{
-           //         System.out.println("User has not selected anything");
-           //     }
-            // If your switch cases don't handle control flow, fall-through will result
-            // This can be advantageous
+                // if (firstInput.equals("1")) {
+                //     System.out.println("User has selected login...");
+                // } else if {
+                //     firstInput.equals("2") {
+                //         System.out.println("User has selected Register...");
+                //     } else if {
+                //         firstInput.equals("3") {
+                //             System.out.println("See you next time!");
+                //         }
+                //
+                //     } else{
+                //         System.out.println("User has not selected anything");
+                //     }
+                // If your switch cases don't handle control flow, fall-through will result
+                // This can be advantageous
 
-            switch (firstInput){
-                case "1":  // if firstInput.equals("1") then this case will execute
-                    System.out.println("User has selected login");
-                    break; // The keyword break prevents fallthrough
+                switch (firstInput) {
+                    case "1":  // if firstInput.equals("1") then this case will execute
+                        System.out.println("User has selected login");
+                        break; // The keyword break prevents fallthrough
 
-                case  "2":
-                    System.out.println("User has selected register");
-                    register(); // register() - the () invokes the method
+                    case "2":
+                        System.out.println("User has selected register");
+                        register(); // register() - the () invokes the method
 
-                    break;
+                        break;
 
                     // case "i": //example of fall through
-                case "3":
-                    System.out.println("See you next time!");
-                    break;
+                    case "3":
+                        System.out.println("User wishes to view other members!");
+                        Member[] members = readFile();
+                        for (Member member : members) {
+                            System.out.println(member);
+                        }
+                        break;
 
-                default:
-                    System.out.println("User has not selected a valid input");
+
+                    case "4":
+                        System.out.println("See you next time!");
+
+                    isRunning = false;
+                        break;
+
+                    default:
+                        System.out.println("User has not selected a valid input");
 
 
+                }
 
-            }
-
-            } catch(IOException e){  //Catches that exception and assigns to variable e
+            } catch (IOException e) {  //Catches that exception and assigns to variable e
                 e.printStackTrace();
             }
         }
+    }
+
 
         public static void register() throws IOException { // Throws + ExceptionName is known as ducking
-                // Ducking means passing the responsibility of handling the exception to the code that's calling it.
-                // Problem... terminalReader is not within this scope... Solution?
-                // We need to define the terminalReader as static outside the main method - see line 22
+            // Ducking means passing the responsibility of handling the exception to the code that's calling it.
+            // Problem... terminalReader is not within this scope... Solution?
+            // We need to define the terminalReader as static outside the main method - see line 22
 
             System.out.println("Please enter your user email: \n>"); // \n is a new line character, aka return or enter
             String email = terminalReader.readLine();
@@ -112,7 +127,7 @@ public class MainDriver {
             File memoryFile = new File("resources/data.txt");
 
             // This is a try-with-resources block. This allows for anything that extends to AutoClosable to be auto closed.
-            try(FileWriter fileWriter = new FileWriter(memoryFile,true);){
+            try (FileWriter fileWriter = new FileWriter(memoryFile, true);) {
 
                 Member member = new Member(email, fullName, Integer.parseInt(experienceMonths), registrationDate);
                 //This is an old, and rather poor way
@@ -128,15 +143,51 @@ public class MainDriver {
 
                 System.out.println("New user has registered: " + member.toString());
 
-               Member member2 = new Member();
+                Member member2 = new Member();
                 fileWriter.write(member.writetoFile());
-            } catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             //fileWriter.close();  //be mindful to ALWAYS close!!
+        }
+
+            public static Member[] readFile(){  //we want to return information from this method
+                Member[] members = new Member[10];
+
+                //try with resources automatically closes files for us
+                try (
+                        FileReader fileReader = new FileReader("resources/data.txt");
+                        BufferedReader reader = new BufferedReader(fileReader);
+                    ){
+                    
+                    String line = reader.readLine();
+                    int index = 0;
+
+                    while(line != null){
+                        String[] info = line.split(",");
+                        Member member = new Member();
+                        member.setEmail(info[0]);
+                        member.setFullName(info[1]);
+                        //Wrapper classes auto box (can convert back to the primitive value)
+                        member.setExperienceMonths(Integer.parseInt(info[2])); //how does the Integer Class become the primitive
+                        member.setRegistrationDate(info[3]);
+                        member.setPassword(info[4]);
+                        members[index] = member; //swapped from String line to Member member. (This requires a string datatype)
 
 
+                        index++;
+                        line = reader.readLine();
+                    }
 
+
+                } catch (IOException e){
+                    e.printStackTrace();
+                } finally{
+                    System.out.println("Hello from the finally block");
+
+                return members;
+
+            }
         }
     }
 
