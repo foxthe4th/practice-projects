945d9b31c04cfe8aa5bee34a07e1220566879095
Pitmon <foxthe4th@gmail.com>
2022-08-25 16:11:58 -0600

    Finished binary numbers coding challenge

--- src/com/revature/nabnak/MainDriver.java
+++ src/com/revature/nabnak/MainDriver.java
@@ -3,6 +3,33 @@ package com.revature.nabnak;
 public class MainDriver {
     public static void main(String[] args) {
 
-    }
+        //Takes a value n, converts it into a binary number, then counts the number of consecutive 1s
+        //Coding challenge from HackerRank!
+         int n = 439;
 
+
+         //take n and find its binary representation
+        char[] binaryN = Integer.toBinaryString(n).toCharArray();
+
+        //Declare an int called result
+        int result = 0;
+        int finalResult = 0;
+
+        //iterate through binaryN
+        for (int i = 0; i < binaryN.length; i++){
+
+            //if charAt(i) is 1
+            result = (binaryN[i] == '0') ? 0 : result + 1;
+
+            //set the max number of 1s from the binary string.
+            if(result>finalResult){
+                finalResult = result;
+            }
+        }
+        //print out the result
+        System.out.println(finalResult);
+    }
 }
+
+
+
--- src/com/revature/nabnak/util/AppState.java
+++
@@ -1,2 +0,0 @@
-package com.revature.nabnak.util;public class AppState {
-}
