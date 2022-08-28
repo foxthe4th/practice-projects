03a7c4b9271612d44f3e2721f41d4a49ec950855
Pitmon <foxthe4th@gmail.com>
2022-08-28 10:14:37 -0600

    Coding challenge from hacker rank, 30 days of code. This challenge covers 2d arrays, how to assign them values, and how to iterate through them

--- src/com/hackerrank/codingchallenges/MainDriver.java
+++ src/com/hackerrank/codingchallenges/MainDriver.java
@@ -1,35 +1,56 @@
 package com.hackerrank.codingchallenges;
 
+import java.io.*;
+import java.util.*;
+
 public class MainDriver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        //Takes a value n, converts it into a binary number, then counts the number of consecutive 1s
-        //Coding challenge from HackerRank!
-         int n = 439;
+        //In this challenge, we are tasked with creating a 6x6 2d array. We then look for the 'hourglasses'
+        //within the array, then find and print the largest sum of all possible hourglasses
+        /*EX: 1 1 1 |
+                1   | ->   = 7
+              1 1 1 |      */
 
 
-         //take n and find its binary representation
-        char[] binaryN = Integer.toBinaryString(n).toCharArray();
+        //We would use this for testing given input. I'm not making 36 ints for 1 test
+        //Scanner scanner = new Scanner(System.in);
 
-        //Declare an int called result
-        int result = 0;
-        int finalResult = 0;
+        //Initialize the 2d array to have 6 rows and 6 columns
+        int [][] testArr = new int [6] [6];
 
-        //iterate through binaryN
-        for (int i = 0; i < binaryN.length; i++){
+        //Iterate through each "cell" for each row and column
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++){
 
-            //if charAt(i) is 1
-            result = (binaryN[i] == '0') ? 0 : result + 1;
-
-            //set the max number of 1s from the binary string.
-            if(result>finalResult){
-                finalResult = result;
+                //Fill the "cells" with values from the scanner for the test.
+                //Again, I'm not making a bunch of ints to test this here.
+                //testArr [i][j] = scanner.nextInt();
             }
         }
-        //print out the result
-        System.out.println(finalResult);
+
+        //Set an int sum to a random, LOW number. This will act as our placeholder to compare the final answer against.
+        int sum = -1000;
+
+        //Iterate through the array for hourglasses. Remember: arrays start at indices 0, and iterative for loops
+        //count [0] [0] as the first iteration. Therefor, we need to search through the array [3] [3] at a time to make
+        //an hourglass, and we do this by stopping the iterative search at [i][j] < [4][4]
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+
+                //Create a new int to hold the max sum of the hourglass being tested.
+                int maxSum = testArr[i][j] + testArr[i] [j+1] + testArr[i] [j+2] + testArr[i + 1 ] [j + 1] + testArr[i+2][j] + testArr[i + 2] [j + 1] + testArr[i + 2] [j + 2];
+
+                //If the max sum is larger than our initial placeholder sum, change the value of the placeholder sum
+                //to be the current max sum.
+                if(maxSum > sum){
+                    sum = maxSum;
+                }
+            }
+        }
+
+        //Finally, print out the max sum
+        System.out.println(sum);
+        //scanner.close();
     }
 }
-
-
-
